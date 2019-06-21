ea8435feee026099be68c61c8d7c82c9aa69c459
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-21 17:58:20 +0100

    Add and register admin bar service

--- bootstrap.php
+++ bootstrap.php
@@ -15,4 +15,5 @@ require_once __DIR__ . '/src/constants.php';
 require_once __DIR__ . '/src/helpers.php';
 
 // Services
+\Helick\GTM\AdminBar::boot();
 \Helick\GTM\Snippet::boot();
--- src/AdminBar.php
+++ src/AdminBar.php
@@ -3,6 +3,9 @@
 namespace Helick\GTM;
 
 use Helick\GTM\Contracts\Bootable;
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+use WP_Admin_Bar;
 
 final class AdminBar implements Bootable
 {
@@ -14,5 +17,52 @@ final class AdminBar implements Bootable
     public static function boot(): void
     {
         $self = new static;
+
+        add_action('admin_bar_menu', [$self, 'extendMenu']);
+    }
+
+    /**
+     * Extend the admin bar menu.
+     *
+     * @param WP_Admin_Bar $adminBar
+     *
+     * @return void
+     */
+    public function extendMenu(WP_Admin_Bar $adminBar): void
+    {
+        $isUserAllowed = current_user_can('manage_options');
+        $isBackend     = is_admin();
+
+        if (!$isUserAllowed || $isBackend) {
+            return;
+        }
+
+        $adminBar->add_menu([
+            'id'    => 'helick-gtm',
+            'title' => sprintf(
+                '<span class="ab-icon dashicons-filter"></span> <span class="ab-label">%s</span>',
+                __('Data Layer', DOMAIN)
+            ),
+        ]);
+
+        $dataLayer = dataLayer();
+
+        // Flatten data layer
+        $iterator = new RecursiveArrayIterator($dataLayer);
+        $iterator = new RecursiveIteratorIterator($iterator);
+
+        $dataLayer = iterator_to_array($iterator);
+
+        foreach ($dataLayer as $key => $value) {
+            $adminBar->add_node([
+                'id'     => sanitize_key('helick-gtm-' . $key),
+                'parent' => 'helick-gtm',
+                'title'  => sprintf(
+                    '<span style="font-weight: bold">%s:</span> %s',
+                    $key,
+                    esc_html($value)
+                ),
+            ]);
+        }
     }
 }
